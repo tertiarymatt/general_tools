9d43414b79bd7ca0db9b05253f1f182efda247de
M.S. Patterson <matthew.patterson@usda.gov>
2019-10-24 11:28:55 -0600

    cleaning up formatting

--- CopyRasters_ByExtent.py
+++ CopyRasters_ByExtent.py
@@ -1,7 +1,8 @@
 import arcpy, os
 
 ## Processing variables - EDIT THESE
-workspace = r"D:\GIS_Database\AK_TCC\DEM\slope" #where all the quarter quads are stored
+workspace = r"D:\GIS_Database\AK_TCC\DEM\slope" 
+#where all the quarter quads are stored
 
 #new folder to put the ouptput resampled images into
 outFolder = "D:\GIS_Database\AK_TCC\DEM\Tanana_slope"
@@ -9,7 +10,8 @@ if not os.path.isdir(outFolder):
 	os.makedirs(outFolder)
 
 #Dummy raster
-#Dummyrast = arcpy.sa.Raster(r'D:\Region2\ImageBound12') #create this and make sure bigger than extent
+#Dummyrast = arcpy.sa.Raster(r'D:\Region2\ImageBound12') 
+#create this and make sure bigger than extent
 
 # Project area to limit unneccessary processing 
 ProjectArea = r'D:\GIS_Database\AK_TCC\Boundaries\Tanana.shp'
@@ -20,13 +22,13 @@ arcpy.env.overwriteOutput = True
 arcpy.CheckOutExtension("Spatial")
 arcpy.env.pyramid = "NONE"
 
-####################################################################################################
+###############################################################################
 ##
 ##
 ## PROCESSING 
 ##
 ##
-####################################################################################################
+###############################################################################
 
 ## Get reference extents
 ##ext1 = Dummyrast.extent
@@ -54,25 +56,18 @@ for rast in allRasters:
 	ymax = ext2.YMax
 
 ## Only process those rasters that intersect the study area
-	if ((xmin >= xminA and xmin <= xmaxA) or (xmax >= xminA and xmax <= xmaxA)) and ((ymin >= yminA and ymin <= ymaxA) or (ymax >= yminA and ymax <= ymaxA)):
+	if ((xmin >= xminA and xmin <= xmaxA) or (xmax >= xminA and xmax <= xmaxA)) 
+	and ((ymin >= yminA and ymin <= ymaxA) or (ymax >= yminA and ymax <= ymaxA)):
 		print 'working on', rast
 	
                 #concatonate the name of the out raster with the out folder
 		outRast = outFolder + "\\" + "slope_" + rast
 		
 		#copy over
-		arcpy.CopyRaster_management(rast, outRast, pixel_type = "32_BIT_FLOAT", nodata_value = "-9999")
+		arcpy.CopyRaster_management(rast, outRast, pixel_type = "32_BIT_FLOAT", 
+		nodata_value = "-9999")
+	
 	else:
 		print rast, 'is not in your study area!'
 
 print 'done with all the rasters in:', workspace
-
-
-
-
-
-
-
-
-
-
